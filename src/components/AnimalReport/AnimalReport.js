cfc74089117ec5310325fa8b647e2788f8dcf9e7
Arveen Adap <159744276+ChewyKookie@users.noreply.github.com>
2024-04-24 03:11:32 -0500

    Update AnimalReport.js

--- src/components/AnimalReport/AnimalReport.js
+++ src/components/AnimalReport/AnimalReport.js
@@ -1,95 +1,181 @@
 import React, { useState } from 'react';
-import './AnimalReport.css'; // Import your CSS file
+import './AnimalReport.css';
 import axios from 'axios';
 
 function AnimalReport() {
-  const [animalSpecies, setAnimalSpecies] = useState('');
-  const [reportData, setReportData] = useState([]);
+    // State for form fields
+    const [animalSpecies, setAnimalSpecies] = useState('');
+    const [dietType, setDietType] = useState('');
+    const [diagnosis, setDiagnosis] = useState('');
+    const [reportType, setReportType] = useState('animal');
+    const [reportData, setReportData] = useState([]);
+    const [isGenerating, setIsGenerating] = useState(false);
+    const [reportGenerated, setReportGenerated] = useState(false); // Track if report has been generated
 
-  const handleFormSubmit = async (event) => {
-    event.preventDefault();
+    const handleFormSubmit = async (event) => {
+        event.preventDefault();
+        setIsGenerating(true);
 
-    console.log("Animal Species:", animalSpecies);
+        // Adjusting fields to send null if blank
+        const adjustedData = {
+            animalSpecies: animalSpecies || null,
+            dietType: dietType || null,
+            diagnosis: diagnosis || null,
+        };
 
-    try {
-      // Post request to fetch report data based on the animal species
-      const response = await axios.post('http://localhost:5095/api/ZooDb/GenerateAnimalReport', {
-        animalSpecies: animalSpecies,
-      }, {
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      });
-      
-      // Set the report data based on the response from the back-end
-      setReportData(response.data);
-    } catch (error) {
-      if (error.response) {
-          console.error('Server responded with:', error.response.status);
-          console.error('Response data:', error.response.data);
-  
-          // Check if errors exist in the response
-          if (error.response.data && error.response.data.errors) {
-              console.error('Validation errors:', error.response.data.errors);
-          }
-      } else {
-          console.error('Error:', error.message);
-      }
-    }
-  };
+        console.log('Request Data:', adjustedData);
 
-  return (
-    <div className="animal-report-container">
-      <h2 className="animal-report-title">Zoo Animal Report</h2>
-      <form onSubmit={handleFormSubmit} className="animal-report-form">
-        <label htmlFor="animalSpecies" className="animal-species-label">Animal Species:</label>
-        <input
-          type="text"
-          id="animalSpecies"
-          value={animalSpecies}
-          onChange={(event) => setAnimalSpecies(event.target.value)}
-          required
-          className="animal-species-input"
-        />
-        <button type="submit" className="generate-report-button">Generate Report</button>
-      </form>
+        try {
+            const response = await axios.get('https://zoodatabasebackend.azurewebsites.net/api/ZooDb/GetAnimalReport', { params: adjustedData });
+            const data = response.data;
 
-      {/* If there is report data, display it in a table */}
-      {reportData.length > 0 && (
-        <div className="report-data">
-          <h3>Animals of matching species:</h3>
-          <table className="report-table">
-            <thead>
-              <tr>
-                <th>Animal ID</th>
-                <th>Name</th>
-                <th>Gender</th>
-                <th>Date of Birth</th>
-                <th>Endangered</th>
-                <th>Date of Arrival</th>
-                <th>Origin</th>
-                <th>Life Stage</th>
-              </tr>
-            </thead>
-            <tbody>
-              {reportData.map((animal, index) => (
-                <tr key={index}>
-                  <td>{animal.animalID}</td>
-                  <td>{animal.animalName}</td>
-                  <td>{animal.animalGender}</td>
-                  <td>{new Date(animal.animalDoB).toLocaleDateString()}</td>
-                  <td>{animal.animalEndangered ? 'Yes' : 'No'}</td>
-                  <td>{new Date(animal.animalDoA).toLocaleDateString()}</td>
-                  <td>{animal.animalOrigin}</td>
-                  <td>{animal.animalLifeStage}</td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
+            console.log('Response Data:', data);
+
+            if (!data || data.length === 0) {
+                alert('No data found!');
+                setReportData([]);
+                return;
+            }
+
+            setReportData(data);
+
+        } catch (error) {
+            console.error('Error fetching data:', error);
+            alert('An error occurred while fetching data!');
+        } finally {
+            setIsGenerating(false);
+            setReportGenerated(true); // Set reportGenerated to true after generating the report
+        }
+    };
+
+    const handleReportTypeChange = (event) => {
+        setReportType(event.target.value);
+        setReportGenerated(false); // Reset reportGenerated when dropdown changes
+    };
+
+    return (
+        <div className="animal-report-container">
+            <form onSubmit={handleFormSubmit}>
+                <div className="form-group">
+                <h2>Animal Report</h2>
+                    <label htmlFor="animalSpecies">Animal Species:</label>
+                    <input
+                        type="text"
+                        id="animalSpecies"
+                        value={animalSpecies}
+                        onChange={(e) => setAnimalSpecies(e.target.value)}
+                        className="input"
+                    />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="dietType">Diet Type:</label>
+                    <input
+                        type="text"
+                        id="dietType"
+                        value={dietType}
+                        onChange={(e) => setDietType(e.target.value)}
+                        className="input"
+                    />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="diagnosis">Diagnosis:</label>
+                    <input
+                        type="text"
+                        id="diagnosis"
+                        value={diagnosis}
+                        onChange={(e) => setDiagnosis(e.target.value)}
+                        className="input"
+                    />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="reportType">Report Type:</label>
+                    <select
+                        id="reportType"
+                        value={reportType}
+                        onChange={handleReportTypeChange}
+                        className="input"
+                    >
+                        <option value="animal">Animal</option>
+                        <option value="diet">Diet</option>
+                        <option value="vet">Vet Records</option>
+                    </select>
+                </div>
+                <button type="submit" className="submit-button" disabled={isGenerating}>
+                    Generate Report
+                </button>
+            </form>
+
+            {/* Display report data if available and if reportGenerated is true */}
+            {reportData.length > 0 && reportGenerated && (
+                <div className="report-data">
+                    <table className="report-table">
+                        <thead>
+                            <tr>
+                                <th>Name</th>
+                                <th>Species</th>
+                                {reportType === 'animal' && (
+                                    <>
+                                        <th>Date of Birth</th>
+                                        <th>Gender</th>
+                                        <th>Endangered</th>
+                                        <th>Date of Arrival</th>
+                                        <th>Origin</th>
+                                    </>
+                                )}
+                                {reportType === 'diet' && (
+                                    <>
+                                        <th>Diet Name</th>
+                                        <th>Diet Type</th>
+                                        <th>Diet Schedule</th>
+                                    </>
+                                )}
+                                {reportType === 'vet' && (
+                                    <>
+                                        <th>Weight</th>
+                                        <th>Height</th>
+                                        <th>Diagnosis</th>
+                                        <th>Medications</th>
+                                    </>
+                                )}
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {reportData.map((item, index) => (
+                                <tr key={index}>
+                                    <td>{item.animal_name}</td>
+                                    <td>{item.animal_species}</td>
+                                    {reportType === 'animal' && (
+                                        <>
+                                            <td>{new Date(item.animal_DoB).toLocaleDateString()}</td>
+                                            <td>{item.animal_gender}</td>
+                                            <td>{item.animal_endangered ? 'Yes' : 'No'}</td>
+                                            <td>{new Date(item.animal_DoA).toLocaleDateString()}</td>
+                                            <td>{item.animal_origin}</td>
+                                        </>
+                                    )}
+                                    {reportType === 'diet' && (
+                                        <>
+                                            <td>{item.diet_name}</td>
+                                            <td>{item.diet_type}</td>
+                                            <td>{item.diet_schedule}</td>
+                                        </>
+                                    )}
+                                    {reportType === 'vet' && (
+                                        <>
+                                            <td>{item.weight}</td>
+                                            <td>{item.height}</td>
+                                            <td>{item.diagnosis}</td>
+                                            <td>{item.medications}</td>
+                                        </>
+                                    )}
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                </div>
+            )}
         </div>
-      )}
-    </div>
-  );
+    );
 }
 
 export default AnimalReport;
